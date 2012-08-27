67ee086f0f14e0921d754200af7d4aa52c1d6c8d
Tor <typografi@gmail.com>
2012-08-27 15:59:23 +0200

    Finished markup for data table

--- raw/raw-table.php
+++ raw/raw-table.php
@@ -1,168 +1,119 @@
-<h1>All content <button class="btn btn-primary">Create new…</button></h1>
 
-<section class="eze-table-actions">
-    <label style="float:left;margin:0 5px 0 0" class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label>
-    <button class="btn disabled">Publish selected</button>
-    <button class="btn disabled">Delete selected</button>
-</section>
 
-<section class="eze-table-sort">
-    <ul class="nav nav-pills">
-        <li><p style="font-size:11px;font-weight:bold;margin: 2px 5px 0 0">Sort by</p></li>
-        <li class="active"><a href="#">Date</a></li>
-        <li><a href="#">Author</a></li>
-        <li><a href="#">Content type</a></li>
-    </ul>
-    <div class="view-options">
-        <i class="icon-th-list"></i>
-        <i class="icon-th-large" style="opacity:0.5"></i>
-        <!--<i class="icon-search"></i>-->
-    </div>
-</section>
-
-<section class="eze-table-filters">
-    hide this
-    <form class="form-search">
-        <div class="input-prepend">
-            <span class="add-on"><i class="icon-search"></i></span><input type="text" placeholder="Type to search" class="input-medium search-query">
+<section class="eze-table">
+    <h1>All content <button class="btn btn-primary">Create new…</button></h1>
+    <section class="eze-table-actions">
+        <input type="checkbox" />
+        <button class="btn disabled">Publish selected</button>
+        <button class="btn disabled">Delete selected</button>
+        <div class="sort-by">
+            <ul class="nav nav-pills">
+                <li><h2>Sort by</h2></li>
+                <li class="active"><a href="#">Date</a></li>
+                <li><a href="#">Author</a></li>
+                <li><a href="#">Content type</a></li>
+            </ul>
+            <img class="active" src="img/24x24/Ordered list 24x24b.png" alt="" />
+            <img src="img/24x24/Thumbnails 24x24.png" alt="" />
         </div>
-    </form>
-    <p class="description">Last edit date</p>
-        <select class="chzn-select"><option>All time</option><option>Last week</option><option>Last month</option></select>
-    <p class="description">Content type</p>
-        <select class="chzn-select"><option>All content types</option><option>Article</option><option>Page</option></select>
-    <p class="description">With status</p>
-        <select class="chzn-select"><option>All statuses</option><option>Published</option><option>Edited</option><option>Drafts</option></select>
-    <p class="description">By author</p>
-        <select class="chzn-select"><option>All authors</option><option>2</option></select>
-    <p class="description">From location</p>
-        <select class="chzn-select"><option>All locations</option><option>Mobile</option><option>iPad</option><option>Web</option></select>
+    </section>
 
-    <h2 style="font-size:12px;margin:10px 0 5px 0;line-height:12px">Saved searches</h2>
-        <span class="label">Grillsandwich</span> <span class="label">Fårikål</span> <span class="label">MatPrat</span>
+    <section class="eze-filters">
+        <form class="form-search"><div class="input-prepend">
+            <span class="add-on"><i class="icon-search"></i></span><input type="text" placeholder="Type to search" class="input-medium">
+        </div></form>
+        <p class="description">Last edit date</p><select class="chzn-select"><option>All time</option><option>Last week</option><option>Last month</option></select>
+        <p class="description">Content type</p><select class="chzn-select"><option>All content types</option><option>Article</option><option>Page</option></select>
+        <p class="description">With status</p><select class="chzn-select"><option>All statuses</option><option>Published</option><option>Edited</option><option>Drafts</option></select>
+        <p class="description">By author</p><select class="chzn-select"><option>All authors</option><option>2</option></select>
+        <p class="description">From location</p><select class="chzn-select"><option>All locations</option><option>Mobile</option><option>iPad</option><option>Web</option></select>
+
+        <h2 style="font-size:12px;margin:10px 0 5px 0;line-height:12px">Saved searches</h2>
+            <span class="label">Grillsandwich</span> <span class="label">Fårikål</span> <span class="label">MatPrat</span>
+    </section>
+
+    <section class="eze-table-content">
+
+        <ul class="items">
+            <li class="group-header">Today</li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+        </ul>
+        <ul>
+            <li class="group-header">Yesterday</li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+            <li class="item clearfix">
+                <label class="check"><input type="checkbox"></label> <img class="thumb" src="img/thumb.png" alt=""> <h2>Grillsandwich with chicken timian and a mix of greens</h2>
+                <p>
+                    <span class="label label-warning">Edited</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
+                    <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                </p>
+            </li>
+
+        </ul>
+
+
+    </section>
+
+
+
+
+
 </section>
 
-<table class="eze-datatable">
-    <thead>
-        <th>Today</th>
-        <th></th>
-        <th>Author</th>
-        <th>Content type</th>
-    </thead>
-    <tbody>
-        <tr>
-            <td class="eze-table-check"><label class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label></td>
-            <td><img src="http://cdn2.matprat.no/sitefiles/site4/files/artimg/flere-vil-spise-sunt5.jpg" style="height: 80px; width: 100px;float:left;margin:0 10px 0 0;" alt=""><a href="#edit/783">
-                <h2 style="font-size:14px;margin:10px 0 0 0;line-height:14px;">Grillsandwich med timiankylling og grønnsaksmiks<br /><span class="label label-success">Published</span>
-                    <span style="font-weight:normal;font-size:11px;color:#555;">13:30 by Trude Henrichsen</span>
-                </h2>
-                <p style="margin:0;line-height:18px;font-size:11px">/presse/nyheter/flere-vil-spise-sunt/ and 3 other locations<br />Norwegian and English</span></p></a>
-            </td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Trude Henrichsen</td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Nyhetsartikkel</td>
-        </tr>
-        <tr>
-            <td class="eze-table-check"><label class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label></td>
-            <td><img src="http://cdn2.matprat.no/sitefiles/site4/files/artimg/flere-vil-spise-sunt5.jpg" style="height: 80px; width: 100px;float:left;margin:0 10px 0 0;" alt=""><a href="#edit/783">
-                <h2 style="font-size:14px;margin:10px 0 0 0;line-height:14px;">Grillsandwich med timiankylling og grønnsaksmiks<br /><span class="label label-warning">Edited</span>
-                    <span style="font-weight:normal;font-size:11px;color:#555;">13:30 by Trude Henrichsen</span>
-                </h2>
-                <p style="margin:0;line-height:18px;font-size:11px">/presse/nyheter/flere-vil-spise-sunt/ and 3 other locations<br />Norwegian and English</span></p></a>
-            </td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Trude Henrichsen</td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Nyhetsartikkel</td>
-        </tr>
-        <tr>
-            <td class="eze-table-check"><label class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label></td>
-            <td><img src="http://cdn2.matprat.no/sitefiles/site4/files/artimg/flere-vil-spise-sunt5.jpg" style="height: 80px; width: 100px;float:left;margin:0 10px 0 0;" alt=""><a href="#edit/783">
-                <h2 style="font-size:14px;margin:10px 0 0 0;line-height:14px;">Grillsandwich med timiankylling og grønnsaksmiks<br /><span class="label label-inverse">Not published</span>
-                    <span style="font-weight:normal;font-size:11px;color:#555;">13:30 by Trude Henrichsen</span>
-                </h2>
-                <p style="margin:0;line-height:18px;font-size:11px">/presse/nyheter/flere-vil-spise-sunt/ and 3 other locations<br />Norwegian and English</span></p></a>
-            </td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Trude Henrichsen</td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Nyhetsartikkel</td>
-        </tr>
-        <tr>
-            <td class="eze-table-check"><label class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label></td>
-            <td><img src="http://cdn2.matprat.no/sitefiles/site4/files/artimg/flere-vil-spise-sunt5.jpg" style="height: 80px; width: 100px;float:left;margin:0 10px 0 0;" alt=""><a href="#edit/783">
-                <h2 style="font-size:14px;margin:10px 0 0 0;line-height:14px;">Grillsandwich med timiankylling og grønnsaksmiks<br /><span class="label label-inverse">Not published</span>
-                    <span style="font-weight:normal;font-size:11px;color:#555;">13:30 by Trude Henrichsen</span>
-                </h2>
-                <p style="margin:0;line-height:18px;font-size:11px">/presse/nyheter/flere-vil-spise-sunt/ and 3 other locations<br />Norwegian and English</span></p></a>
-            </td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Trude Henrichsen</td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Nyhetsartikkel</td>
-        </tr>
-    </tbody>
-</table>
 
 
-
-<table class="eze-datatable">
-    <thead>
-        <th>Last week</th>
-        <th></th>
-        <th></th>
-        <th></th>
-    </thead>
-    <tbody>
-        <tr>
-            <td class="eze-table-check"><label class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label></td>
-            <td><img src="http://cdn2.matprat.no/sitefiles/site4/files/artimg/flere-vil-spise-sunt5.jpg" style="height: 80px; width: 100px;float:left;margin:0 10px 0 0;" alt=""><a href="#edit/783">
-                <h2 style="font-size:14px;margin:10px 0 0 0;line-height:14px;">Grillsandwich med timiankylling og grønnsaksmiks<br /><span class="label label-success">Published</span>
-                    <span style="font-weight:normal;font-size:11px;color:#555;">13:30 by Trude Henrichsen</span>
-                </h2>
-                <p style="margin:0;line-height:18px;font-size:11px">/presse/nyheter/flere-vil-spise-sunt/ and 3 other locations<br />Norwegian and English</span></p></a>
-            </td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Trude Henrichsen</td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Nyhetsartikkel</td>
-        </tr>
-        <tr>
-            <td class="eze-table-check"><label class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label></td>
-            <td><img src="http://cdn2.matprat.no/sitefiles/site4/files/artimg/flere-vil-spise-sunt5.jpg" style="height: 80px; width: 100px;float:left;margin:0 10px 0 0;" alt=""><a href="#edit/783">
-                <h2 style="font-size:14px;margin:10px 0 0 0;line-height:14px;">Grillsandwich med timiankylling og grønnsaksmiks<br /><span class="label label-success">Published</span>
-                    <span style="font-weight:normal;font-size:11px;color:#555;">13:30 by Trude Henrichsen</span>
-                </h2>
-                <p style="margin:0;line-height:18px;font-size:11px">/presse/nyheter/flere-vil-spise-sunt/ and 3 other locations<br />Norwegian and English</span></p></a>
-            </td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Trude Henrichsen</td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Nyhetsartikkel</td>
-        </tr>
-        <tr>
-            <td class="eze-table-check"><label class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label></td>
-            <td><img src="http://cdn2.matprat.no/sitefiles/site4/files/artimg/flere-vil-spise-sunt5.jpg" style="height: 80px; width: 100px;float:left;margin:0 10px 0 0;" alt=""><a href="#edit/783">
-                <h2 style="font-size:14px;margin:10px 0 0 0;line-height:14px;">Grillsandwich med timiankylling og grønnsaksmiks<br /><span class="label label-success">Published</span>
-                    <span style="font-weight:normal;font-size:11px;color:#555;">13:30 by Trude Henrichsen</span>
-                </h2>
-                <p style="margin:0;line-height:18px;font-size:11px">/presse/nyheter/flere-vil-spise-sunt/ and 3 other locations<br />Norwegian and English</span></p></a>
-            </td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Trude Henrichsen</td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Nyhetsartikkel</td>
-        </tr>
-        <tr>
-            <td class="eze-table-check"><label class="eze-tap"><input type="checkbox" name="gridCheckCell[]"></label></td>
-            <td><img src="http://cdn2.matprat.no/sitefiles/site4/files/artimg/flere-vil-spise-sunt5.jpg" style="height: 80px; width: 100px;float:left;margin:0 10px 0 0;" alt=""><a href="#edit/783">
-                <h2 style="font-size:14px;margin:10px 0 0 0;line-height:14px;">Grillsandwich med timiankylling og grønnsaksmiks<br /><span class="label label-success">Published</span>
-                    <span style="font-weight:normal;font-size:11px;color:#555;">13:30 by Trude Henrichsen</span>
-                </h2>
-                <p style="margin:0;line-height:18px;font-size:11px">/presse/nyheter/flere-vil-spise-sunt/ and 3 other locations<br />Norwegian and English</span></p></a>
-            </td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Trude Henrichsen</td>
-            <td style="vertical-align:top;padding-top:15px;font-size:11px">Nyhetsartikkel</td>
-        </tr>
-    </tbody>
-</table>
-
-
-
-
-
-<!--<table class="eze-datatable" style="margin: 50px 0 0 0">
-    <tbody>
-        <tr class="eze-table-no-hits">
-            <td>No content found ☃</td>
-        </tr>
-    </tbody>
-</table> -->
 
