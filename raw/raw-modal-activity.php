1d74dbc66e48f5cf06e896c192d3d16aad53f739
Tor <typografi@gmail.com>
2012-09-10 15:23:40 +0200

    Updated activity with new default user, and like links

--- raw/raw-modal-activity.php
+++ raw/raw-modal-activity.php
@@ -2,35 +2,38 @@
 <span class="arrow"><i class="icon-caret-left"></i></span>
     <h1>Activity</h1>
         <div class="btn-group" data-toggle="buttons-radio">
-            <button class="btn">This page</button>
-            <button class="btn">All</button>
+            <button class="btn active">This page</button>
+            <button class="btn">All activity</button>
             <button class="btn">@me</button>
-            <button class="btn">Pinned</button>
         </div>
         <div class="input-append">
-            <input class="span2" id="appendedInputButton" size="16" type="text" placeholder="Write message…"><button class="btn" type="button">Share</button>
+            <input id="appendedInputButton" size="16" type="text" placeholder="Write message…"><button class="btn" type="button">Share</button>
         </div>
             <ol class="eze-comments">
                 <li class="clearfix">
-                    <img src="img/default-user.jpeg">
-                    <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Mutley, you snickering, floppy eared hound. When courage is needed, you're never around. Those medals you wear on your moth-eaten chest should be there for bungling at which you are best.</p>
+                    <img class="user-image" src="img/default-user.jpeg">
+                    <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Mutley, you snickering, floppy eared hound. When courage is needed, you're never around. Those medals you wear on your moth-eaten chest should be there for bungling at which you are best.</p>
+                    <p class="like-post"><a href="#">Like</a> 10 people like this</p>
                     <button class="btn btn-mini add-comment-button">Comment</button>
                 </li>
                 <li class="clearfix">
-                    <img src="img/default-user.jpeg">
+                    <img class="user-image" src="img/default-user.jpeg">
                     <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Hello world</p>
+                    <p class="like-post"><a href="#">Like</a></p>
                     <button class="btn btn-mini add-comment-button">Comment</button>
                 </li>
                 <li class="clearfix">
-                    <img src="img/default-user.jpeg">
-                    <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Hello world</p>
+                    <img class="user-image" src="img/default-user.jpeg">
+                    <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Hello world</p>
+                    <p class="like-post hide"><a href="#">Like</a> 10 people like this</p>
                     <div class="input-append add-comment">
                         <input id="appendedInputButton" type="text" placeholder="Write message…"><button class="btn btn-mini" type="button">Post</button>
                     </div>
                 </li>
                 <li class="clearfix">
-                    <img src="img/default-user.jpeg">
-                    <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Hello world</p>
+                    <img class="user-image" src="img/default-user.jpeg">
+                    <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Hello world</p>
+                    <p class="like-post"><a href="#">Like</a></p>
                     <button class="btn btn-mini add-comment-button">Comment</button>
                 </li>
             </ol>
--- raw/raw-object-sidebar.php
+++ raw/raw-object-sidebar.php
@@ -30,27 +30,31 @@
 			</div>
 			<ol class="eze-comments">
 			    <li class="clearfix">
-		            <img src="img/default-user.jpeg">
-		            <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Mutley, you snickering, floppy eared hound. When courage is needed, you're never around. Those medals you wear on your moth-eaten chest should be there for bungling at which you are best.</p>
-			        <button class="btn btn-mini add-comment-button">Comment</button>
-			    </li>
-			    <li class="clearfix">
-		            <img src="img/default-user.jpeg">
-		            <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Hello world</p>
-			        <button class="btn btn-mini add-comment-button">Comment</button>
-			    </li>
-			    <li class="clearfix">
-		            <img src="img/default-user.jpeg">
-		            <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Hello world</p>
-		            <div class="input-append add-comment">
-                		<input id="appendedInputButton" type="text" placeholder="Write message…"><button class="btn btn-mini" type="button">Post</button>
-					</div>
-			    </li>
-			    <li class="clearfix">
-		            <img src="img/default-user.jpeg">
-		            <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Hello world</p>
-		            <button class="btn btn-mini add-comment-button">Comment</button>
-			    </li>
+                    <img class="user-image" src="img/default-user.jpeg">
+                    <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Mutley, you snickering, floppy eared hound. When courage is needed, you're never around. Those medals you wear on your moth-eaten chest should be there for bungling at which you are best.</p>
+                    <p class="like-post"><a href="#">Like</a> 10 people like this</p>
+                    <button class="btn btn-mini add-comment-button">Comment</button>
+                </li>
+                <li class="clearfix">
+                    <img class="user-image" src="img/default-user.jpeg">
+                    <p><span class="date">35 days ago, from <strong>Jon Helge Stensrud</strong></span><br>Hello world</p>
+                    <p class="like-post"><a href="#">Like</a></p>
+                    <button class="btn btn-mini add-comment-button">Comment</button>
+                </li>
+                <li class="clearfix">
+                    <img class="user-image" src="img/default-user.jpeg">
+                    <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Hello world</p>
+                    <p class="like-post hide"><a href="#">Like</a> 10 people like this</p>
+                    <div class="input-append add-comment">
+                        <input id="appendedInputButton" type="text" placeholder="Write message…"><button class="btn btn-mini" type="button">Post</button>
+                    </div>
+                </li>
+                <li class="clearfix">
+                    <img class="user-image" src="img/default-user.jpeg">
+                    <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Hello world</p>
+                    <p class="like-post"><a href="#">Like</a></p>
+                    <button class="btn btn-mini add-comment-button">Comment</button>
+                </li>
 			</ol>
 
 
