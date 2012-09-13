7c77d944d2faba6f4b276a94dbfe5b350c940e88
Tor <typografi@gmail.com>
2012-09-13 13:35:23 +0200

    hover and active graphics for the toolbar, needs a active class on the LI

--- raw/raw-toolbar.php
+++ raw/raw-toolbar.php
@@ -2,7 +2,7 @@
     <ul>
         <li><a href="?dashboard"><b class="eze-settings"></b>Username</a></li>
         <li><a href="?addcontent"><b class="eze-create"></b>Create</a></li>
-        <li><a href="?view=attributes"><b class="eze-document"></b>Edit</a></li>
+        <li class="active"><a href="?view=attributes"><b class="eze-document"></b>Edit</a></li>
         <li><a href="?view=table"><b class="eze-search"></b>Find</a>
             <div class="slide-in quick-action search">
                 <div class="input-prepend">
@@ -11,7 +11,7 @@
                <button class="btn">View all content</button>
             </div>
         </li>
-        <li><a href="?publish"><b class="eze-publish"></b><span class="status-icon changed"></span>Publish</a></li>
+        <li><a href="?publish"><b class="eze-publish"></b><!--<span class="status-icon changed"></span> -->Publish</a></li>
         <li><a href="?activity"><b class="eze-activity"></b>Activity</a></li>
         <li><a href="?devices"><b class="eze-devices"></b>Devices</a></li>
         <li><a href="?view=sitemap"><b class="eze-sitemap"></b>Sitemap</a></li>
