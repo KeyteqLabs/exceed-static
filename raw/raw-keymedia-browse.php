eb334669597b7bc9f722f647cd8484365e853134
Tor <typografi@gmail.com>
2012-09-14 10:40:24 +0200

    Added thumb browse view to keymedia-browse

--- raw/raw-keymedia-browse.php
+++ raw/raw-keymedia-browse.php
@@ -3,28 +3,194 @@
   <button type="submit" class="btn">Search for media</button>
 </form>
 
-<ul class="thumbnails">
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-	<li><a href="#" class="thumbnail"><img src="http://placehold.it/180x180" alt=""></a></li>
-</ul>
+<div class="keymedia-thumbs">
+
+    <div class="item">
+        <a class="pick" data-id="503f4a584c6ac0645201200b">
+            <img src="http://demo.cdn.keymedia.no/160x120/503f4a584c6ac0645201200b.jpg">
+            <p class="meta">Wanted-Art-Director-68-9<br><span class="details">1024 x 768</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4fc86c7d4c6ac0bf2a0005a5">
+            <img src="http://demo.cdn.keymedia.no/160x120/4fc86c7d4c6ac0bf2a0005a5.jpg">
+            <p class="meta">Getting-Started-with-e-Z-Publish-Etna-69-7<br><span class="details">1600 x 1000</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4fc4dca84c6ac0120c000797">
+            <img src="http://demo.cdn.keymedia.no/160x120/4fc4dca84c6ac0120c000797.jpg">
+            <p class="meta">Winter.jpg<br><span class="details">800 x 600</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4fa26db44c6ac05a07000135">
+            <img src="http://demo.cdn.keymedia.no/160x120/4fa26db44c6ac05a07000135.jpg">
+            <p class="meta">Quarterly-Report-Q4-236-12<br><span class="details">3813 x 2729</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4fa26d7c4c6ac04e740004be">
+            <img src="http://demo.cdn.keymedia.no/160x120/4fa26d7c4c6ac04e740004be.jpg">
+            <p class="meta">Quarterly-Report-Q2-245-11<br><span class="details">1720 x 1244</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4fa26d3a4c6ac00151000b2c">
+            <img src="http://demo.cdn.keymedia.no/160x120/4fa26d3a4c6ac00151000b2c.jpg">
+            <p class="meta">Our-products-6180-7<br><span class="details">3648 x 2736</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4fa26d074c6ac054740004b4">
+            <img src="http://demo.cdn.keymedia.no/160x120/4fa26d074c6ac054740004b4.jpg">
+            <p class="meta">Quarterly-Report-Q1-240-16<br><span class="details">1024 x 825</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4fa26cdb4c6ac02250000b33">
+            <img src="http://demo.cdn.keymedia.no/160x120/4fa26cdb4c6ac02250000b33.jpg">
+            <p class="meta">Quarterly-Report-Q3-2715-6<br><span class="details">2416 x 2131</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f384c6ac0e6500001b0">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f384c6ac0e6500001b0.jpg">
+            <p class="meta">Photo – Copenhagen.jpg<br><span class="details">600 x 800</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f384c6ac042500001b1">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f384c6ac042500001b1.jpg">
+            <p class="meta">Photo – Copenhagen 3.jpg<br><span class="details">600 x 800</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f374c6ac09f5900187d">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f374c6ac09f5900187d.jpg">
+            <p class="meta">Photo – Copenhagen 2.jpg<br><span class="details">600 x 800</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f374c6ac0f43a001a8f">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f374c6ac0f43a001a8f.jpg">
+            <p class="meta">Photo – Colour.jpg<br><span class="details">600 x 713</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f364c6ac0a4500001b3">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f364c6ac0a4500001b3.jpg">
+            <p class="meta">Photo – Church.jpg<br><span class="details">600 x 800</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f354c6ac0cf4600033f">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f354c6ac0cf4600033f.jpg">
+            <p class="meta">Photo – Chemicals.jpg<br><span class="details">1000 x 666</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f324c6ac0953a001a92">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f324c6ac0953a001a92.jpg">
+            <p class="meta">Photo – BW Nature.jpg<br><span class="details">900 x 600</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f2e4c6ac0a4500001ae">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f2e4c6ac0a4500001ae.jpg">
+            <p class="meta">Photo – Braun.jpeg<br><span class="details">1000 x 666</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f2e4c6ac0cf4600033a">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f2e4c6ac0cf4600033a.png">
+            <p class="meta">Photo – Birch.png<br><span class="details">990 x 660</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f2c4c6ac0773a001a79">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f2c4c6ac0773a001a79.jpg">
+            <p class="meta">Photo – Big Blue Marble.jpg<br><span class="details">8000 x 8000</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+
+
+    <div class="item">
+        <a class="pick" data-id="4f9a5f244c6ac05d3c0006af">
+            <img src="http://demo.cdn.keymedia.no/160x120/4f9a5f244c6ac05d3c0006af.jpg">
+            <p class="meta">Photo – Belly of the Whale.jpg<br><span class="details">761 x 600</span></p>
+            <span class="share"></span>
+        </a>
+    </div>
+
+</div>
