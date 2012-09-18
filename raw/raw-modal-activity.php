b0018803ff024acd1b9e7cc96ea0d1ef81dae35d
Tor <typografi@gmail.com>
2012-09-18 09:51:35 +0200

    Updates to activity

--- raw/raw-modal-activity.php
+++ raw/raw-modal-activity.php
@@ -15,19 +15,19 @@
                     <li class="clearfix">
                         <img class="user-image" src="img/default-user.jpeg">
                         <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Mutley, you snickering, floppy eared hound. When courage is needed, you're never around. Those medals you wear on your moth-eaten chest should be there for bungling at which you are best.</p>
-                        <p class="like-post"><a href="#">Like</a> 10 people like this</p>
+                        <p class="post-actions"><a href="#">Like</a> 10 people like this</p>
                         <button class="btn btn-mini add-comment-button">Comment</button>
                     </li>
                     <li class="clearfix">
                         <img class="user-image" src="img/default-user.jpeg">
-                        <p><span class="date">35 days ago, from <strong>Your user</strong></span><br>I cannot like my own comment, but I can delete it.</p>
-                        <p class="like-post"><a href="#">Delete</a></p>
+                        <p><span class="date"><strong>Your user</strong> 35 days ago</span><br>I cannot like my own comment, but I can delete it.</p>
+                        <p class="post-actions"><a href="#">Delete</a></p>
                         <button class="btn btn-mini add-comment-button">Comment</button>
                     </li>
                     <li class="clearfix">
                         <img class="user-image" src="img/default-user.jpeg">
                         <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Hello world</p>
-                        <p class="like-post hide"><a href="#">Like</a> 10 people like this</p>
+                        <p class="post-actions hide"><a href="#">Like</a> 10 people like this</p>
                         <div class="input-append add-comment">
                             <input id="appendedInputButton" type="text" placeholder="Write message…"><button class="btn btn-mini" type="button">Post</button>
                         </div>
@@ -35,7 +35,7 @@
                     <li class="clearfix">
                         <img class="user-image" src="img/default-user.jpeg">
                         <p><span class="date"><strong>Jon Helge Stensrud</strong> 35 days ago</span><br>Hello world</p>
-                        <p class="like-post"><a href="#">Like</a></p>
+                        <p class="post-actions"><a href="#">Like</a></p>
                         <button class="btn btn-mini add-comment-button">Comment</button>
                     </li>
                 </ol>
