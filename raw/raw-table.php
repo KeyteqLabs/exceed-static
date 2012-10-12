fbaeefbb25f20c0c6d81c0fe145a704edee177b8
Tor <typografi@gmail.com>
2012-10-12 09:06:50 +0200

    Added view this page to table items, and placed labels on same line

--- raw/raw-table.php
+++ raw/raw-table.php
@@ -74,8 +74,8 @@
                     </div>
                     <h2><a href="#">Grillsandwich with chicken timian and a mix of greens</a></h2>
                     <p>
-                        <span class="label label-success">Published</span> 13:30 by Trude Henrichsen, in Norwegian and English<br />
-                        <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in /presse/nyheter/flere-vil-spise-sunt/ and <span class="other-locations" title="first tooltip">3 other locations</span>
+                        <span class="label label-success">Published</span> 28. Mar. 2012 by Trude Henrichsen <span class="label label-info">You edited</span> 05. Oct. 2012<br />
+                        <img src="img/16x16/Globe alt 16x16.png" alt="" /> World news article, published in <span class="other-locations" title="first tooltip">3 other locations</span> <a href="#">View this page</a>
                     </p>
                 </li>
             <?php } ?>
