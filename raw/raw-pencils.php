ee9b139e063e6085e3a400fc505cde4a194d9eb3
Tor <typografi@gmail.com>
2012-10-23 09:59:55 +0200

    New design for black pencil

--- raw/raw-pencils.php
+++ raw/raw-pencils.php
@@ -3,14 +3,14 @@
 <div class="kp kp-edit-block expanded" title="">
 	<div class="kp-edit-block-box active" style="top: 100px; left: 400px;">
 		<ul class="kp-edit-block-elements">
-			<li class="edit"><a href="#editBlock/60/0/ea8d0512b74249493a30c0ac996b33f0/nor-NO">View settings for «Innhold»</a></li>
-			<li data-id="84"><a href="#edit/84" class="kp-edit-node"><span>Edit</span> «Før du reiser»</a></li>
-			<li data-id="646"><a href="#edit/646" class="kp-edit-node"><span>Edit</span> «Trafikkmeldingar»</a></li>
-			<li data-id="85"><a href="#edit/85" class="kp-edit-node"><span>Edit</span> «Kundesenter»</a></li>
-			<li data-id="86"><a href="#edit/86" class="kp-edit-node"><span>Edit</span> «Viktig melding!»</a></li>
+			<li><a href="#"><img src="img/16x16/white/Add.png" alt=""><strong>Add</strong> content to this block</a></li>
+			<li><a href="#"><img src="img/16x16/white/Edit.png" alt=""><strong>Edit</strong> «Getting Started with eZ Publish Etna»</a></li>
+			<li><a href="#"><img src="img/16x16/white/Edit.png" alt=""><strong>Edit</strong> «The Biggest Mountain in Europe»</a></li>
+			<li><a href="#"><img src="img/16x16/white/Settings.png" alt=""><strong>Settings</strong> for this block</a></li>
 		</ul>
 	</div>
 </div>
+
 <div class="kp kp-edit-block expanded" title="">
 	<div class="kp-edit-block-box" style="top: 40px; left: 400px;">
 		<ul class="kp-edit-block-elements">
