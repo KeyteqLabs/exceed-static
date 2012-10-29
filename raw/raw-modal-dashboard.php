c96f522d2049e5d316749e6372807e25e7f3f13e
Tor <typografi@gmail.com>
2012-10-29 13:14:59 +0100

    Design for profile popover

--- raw/raw-modal-dashboard.php
+++ raw/raw-modal-dashboard.php
@@ -1,28 +1,29 @@
 <div class="popover right eze-dashboard-modal" style="top: 20px; left: 69px; z-index: 75000; display: block;">
 	<div class="arrow"></div>
 	<div class="popover-inner">
-		<h3 class="popover-title">Username</h3>
+		<h3 class="popover-title">Your profile</h3>
 		<div class="popover-content">
 
 
 
-			
-				<div class="well well-small">
-					<!--<div class="alert alert-error">
+				<!--
+					<div class="alert alert-error">
 						<button type="button" class="close" data-dismiss="alert">×</button>
 						<strong>Warning!</strong> Best check yo self, you're not looking too good.
 					</div> -->
-					<h2>Your profile</h2>
 					<form class="form-inline">
 						<input type="text" class="input-small" placeholder="First name">
 						<input type="text" class="input-small" placeholder="Last name">
+						<div class="well well-small">
 						<input type="text" class="input-small" placeholder="Username">
 						<input type="text" class="input-small" placeholder="Email address">
 						<input type="text" class="input-small" placeholder="Password">
 						<input type="text" class="input-small" placeholder="Repeat password">
 						<button type="submit" class="btn btn-primary">Update login</button>
-				</div>
-			</form>
+						</div>
+					</form>
+
+				
 
 			<h2><img src="img/24x24/Time 24x24.png" />Recent</h2>
 				<ul class="eze-items">
@@ -40,6 +41,7 @@
 					<li><div class="wrap"><img src="img/16x16/Document 16x16.png" /><small>Class</small> Object name<div class="right-align"><a class="btn btn-mini edit-btn" href="">Restore</a></div></div></li>
 					<li><div class="wrap"><img src="img/16x16/Document 16x16.png" /><small>Class</small> Object name<div class="right-align"><a class="btn btn-mini edit-btn" href="">Restore</a></div></div></li>
 				</ul>
+			<button class="btn">Login to eZ Publish Administration Interface</button>
 
 
 
