79eb1077c91aacf0e32c4093d0d0f9e28b9fe7aa
Tor <typografi@gmail.com>
2012-11-07 08:41:23 +0100

    Fix for long names in create content

--- raw/raw-modal-add-content.php
+++ raw/raw-modal-add-content.php
@@ -4,7 +4,7 @@
 		<h3 class="popover-title">Create new content</h3>
 		<div class="popover-content">
 			<ul class="eze-content-types">
-				<li><img src="img/16x16/Globe 16x16.png" />Article</li>
+				<li><img src="img/16x16/Globe 16x16.png" />Article with a really really really long name</li>
 				<li><img src="img/16x16/Globe 16x16.png" />Article</li>
 				<li><img src="img/16x16/Globe 16x16.png" />Article</li>
 				<li><img src="img/16x16/Globe 16x16.png" />Article</li>
