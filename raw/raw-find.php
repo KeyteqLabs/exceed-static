ae1647ea487af39fc90242ed8590090798e26483
Tor <typografi@gmail.com>
2013-01-07 09:19:17 +0100

    Markup for find modal

---
+++ raw/raw-find.php
@@ -0,0 +1,17 @@
+<div class="eze-modal-backdrop popover">
+	<section class="eze-modal popover-inner">
+		<nav class="eze-modal-nav">
+			<ul>
+				<li class="active">Content</li>
+				<li>Media</li>
+			</ul>
+		</nav>
+		<span class="gfx"></span>
+		<section class="eze-modal-content"></section>
+		<section class="eze-modal-action">
+			<input type="text" class="empty" />
+			<button class="btn disabled">Details…</button>
+			<button class="btn btn-primary disabled">Insert link</button>
+		</section>
+	</section>
+</div>
--- raw/raw-toolbar.php
+++ raw/raw-toolbar.php
@@ -1,7 +1,7 @@
 <nav id="eze-toolbar">
     <ul>
-        <li><a href="?dashboard"><b class="eze-settings"></b><span class="title">Usernameislongreally</span></a></li>
-        <li><a href="?addcontent"><b class="eze-create"></b>Create</a></li>
+        <li><a href="?dashboard&nostack&nopencils"><b class="eze-settings"></b><span class="title">Usernameislongreally</span></a></li>
+        <li><a href="?addcontent&nostack&nopencils"><b class="eze-create"></b>Create</a></li>
         <li class="active"><a href="?view=attributes"><b class="eze-document"></b>Edit</a></li>
         <li><a href="?view=table"><b class="eze-search"></b>Find</a>
             <div class="slide-in quick-action search">
@@ -11,9 +11,9 @@
                <button class="btn">View all content</button>
             </div>
         </li>
-        <li><a href="?publish"><b class="eze-publish"></b><span class="status-icon published"></span>Publish</a></li>
-        <li><a href="?activity"><b class="eze-activity"></b>Activity</a></li>
-        <li><a href="?preview"><b class="eze-preview"></b>Preview</a></li>
+        <li><a href="?publish&nostack&nopencils"><b class="eze-publish"></b><span class="status-icon published"></span>Publish</a></li>
+        <li><a href="?activity&nostack&nopencils"><b class="eze-activity"></b>Activity</a></li>
+        <li><a href="?preview&nostack&nopencils"><b class="eze-preview"></b>Preview</a></li>
         <li><a href="?view=sitemap"><b class="eze-sitemap"></b>Sitemap</a></li>
         <li><a href="?view=layout"><b class="eze-layout"></b>Zones</a></li>
         <!--
